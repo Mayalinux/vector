8e0f76fe144c18a833b037fb8533d6187c7b641c
Alfredo Mungo <alfredo.mungo@yandex.com>
2014-10-02 01:23:55 +0200

    Added support for automated build scripts

--- scripts/dirs.inc.sh
+++ scripts/dirs.inc.sh
@@ -3,19 +3,19 @@
 ###
 
 # Packages directory
-# This directory contains ready-to-deploy packages
-PKG_DIR=$(dirname "$0")/../pkgs
+# This directory contains ready-to-deploy packages (each in its stage directory)
+PKG_DIR=$(realpath $(dirname "$0"))/../pkgs
 
 # Environment directory
 # This directory contains a virtual environment made up by the packages in
 # $PKG_DIR
-ENV_DIR=$(dirname "$0")/../env
+ENV_DIR=$(realpath $(dirname "$0"))/../env
 
 # Stage directory
 # This directory contains symlinks to the package files which need to be
 # compiled in order to complete the current stage. Each symlink should be
 # removed once its relative package is put in the $PKG_DIR directory
-STG_DIR=$(dirname "$0")/../stage
+STG_DIR=$(realpath $(dirname "$0"))/../stage
 
 # Build directory
 # This directory contains the files and directories that will be included
@@ -24,4 +24,21 @@ BLD_DIR=/tmp/pkg
 
 # Builds directory
 # This directory contains all the build files
-BUILDS_DIR=$(dirname "$0")/../builds
+BUILDS_DIR=$(realpath $(dirname "$0"))/../builds
+
+# Cache directory
+# This directory contains the downloaded files of every package
+CACHE_DIR=$(realpath $(dirname "$0"))/../cache
+
+# Build directory
+# This directory contains the source files at compile time
+BUILD_DIR=$(realpath $(dirname "$0"))/../build
+
+# Patch directory
+# This directory contains all the custom patch files
+PATCH_DIR=$(realpath $(dirname "$0"))/../patches
+
+# Other directory
+# This directory contains any other file that isn't suitable to be contained
+# in one of the other directories
+OTHER_DIR=$(realpath $(dirname "$0"))/../other
