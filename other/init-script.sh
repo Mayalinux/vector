7af7d2db5dda78912061c60987068bc47a7f6ea4
Alfredo Mungo <alfredo.mungo@yandex.com>
2014-10-05 12:25:59 +0200

    Improved scripts

--- other/init-script.sh
+++ other/init-script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin"
+
+exec /bin/bash
+
 mount -t devtmpfs none /dev
 mkdir -p /dev/{shm,pts}
 
@@ -7,5 +11,7 @@ mount -t proc none /proc
 mount -t tmpfs none /run
 mount -t tmpfs none /dev/shm
 mount -t devpts none /dev/pts
+
+echo TEST!
 
 exec /bin/bash
