edc998a2a8adbbb6285f058201ca0e27904da354
Alfredo Mungo <alfredo.mungo@yandex.com>
2014-10-07 01:07:12 +0200

    Refactoring

--- scripts/dirs.inc.sh
+++ scripts/dirs.inc.sh
@@ -2,25 +2,19 @@
 ### This library contains the common directories
 ###
 
-# Packages directory
+# Stages directory
 # This directory contains ready-to-deploy packages (each in its stage directory)
-PKG_DIR=$(realpath $(dirname "$0"))/../pkgs
+STAGES_DIR=$(realpath $(dirname "$0"))/../pkgs
 
 # Environment directory
 # This directory contains a virtual environment made up by the packages in
-# $PKG_DIR
+# $STAGES_DIR
 ENV_DIR=$(realpath $(dirname "$0"))/../env
 
-# Stage directory
-# This directory contains symlinks to the package files which need to be
-# compiled in order to complete the current stage. Each symlink should be
-# removed once its relative package is put in the $PKG_DIR directory
-STG_DIR=$(realpath $(dirname "$0"))/../stage
-
-# Build directory
+# Installation directory
 # This directory contains the files and directories that will be included
 # in the current package.
-BLD_DIR=/tmp/pkg
+INSTALL_DIR=/tmp/pkg
 
 # Builds directory
 # This directory contains all the build files
@@ -36,7 +30,7 @@ BUILD_DIR=$(realpath $(dirname "$0"))/../build
 
 # Patch directory
 # This directory contains all the custom patch files
-PATCH_DIR=$(realpath $(dirname "$0"))/../patches
+PATCHES_DIR=$(realpath $(dirname "$0"))/../patches
 
 # Other directory
 # This directory contains any other file that isn't suitable to be contained
