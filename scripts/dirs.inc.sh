d1cfc7564984f71507e5a0cb12b3ed3fab7ff2db
Alfredo Mungo <alfredo.mungo@yandex.com>
2014-10-11 13:21:55 +0200

    Added support for listing library dependencies of packages

--- scripts/dirs.inc.sh
+++ scripts/dirs.inc.sh
@@ -4,12 +4,12 @@
 
 # Stages directory
 # This directory contains ready-to-deploy packages (each in its stage directory)
-STAGES_DIR=$(realpath $(dirname "$0"))/../pkgs
+STAGES_DIR=$(realpath -L -P $(dirname "$0"))/../pkgs
 
 # Environment directory
 # This directory contains a virtual environment made up by the packages in
 # $STAGES_DIR
-ENV_DIR=$(realpath $(dirname "$0"))/../env
+ENV_DIR=$(realpath -L -P $(dirname "$0"))/../env
 
 # Installation directory
 # This directory contains the files and directories that will be included
@@ -18,25 +18,29 @@ INSTALL_DIR=/tmp/pkg
 
 # Builds directory
 # This directory contains all the build files
-BUILDS_DIR=$(realpath $(dirname "$0"))/../builds
+BUILDS_DIR=$(realpath -L -P $(dirname "$0"))/../builds
 
 # Cache directory
 # This directory contains the downloaded files of every package
-CACHE_DIR=$(realpath $(dirname "$0"))/../cache
+CACHE_DIR=$(realpath -L -P $(dirname "$0"))/../cache
 
 # Build directory
 # This directory contains the source files at compile time
-BUILD_DIR=$(realpath $(dirname "$0"))/../build
+BUILD_DIR=$(realpath -L -P $(dirname "$0"))/../build
 
 # Patch directory
 # This directory contains all the custom patch files
-PATCHES_DIR=$(realpath $(dirname "$0"))/../patches
+PATCHES_DIR=$(realpath -L -P $(dirname "$0"))/../patches
 
 # Other directory
 # This directory contains any other file that isn't suitable to be contained
 # in one of the other directories
-OTHER_DIR=$(realpath $(dirname "$0"))/../other
+OTHER_DIR=$(realpath -L -P $(dirname "$0"))/../other
 
 # Initramfs directory
 # This directory contains the files that will be added to the initramfs
-INITRAMFS_DIR=$(realpath $(dirname "$0"))/../initramfs
+INITRAMFS_DIR=$(realpath -L -P $(dirname "$0"))/../initramfs
+
+# Shared object dependencies log file
+# This file contains information about the shared objects needed for a package
+DEP_LOGFILE=/tmp/pkg_dependencies.log
