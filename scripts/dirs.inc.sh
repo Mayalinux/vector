dd3a33c763cc7ef3dc33944a55747223ad1692b2
Alfredo Mungo <alfredo.mungo@yandex.com>
2014-10-12 13:45:55 +0200

    Added support for package database

--- scripts/dirs.inc.sh
+++ scripts/dirs.inc.sh
@@ -2,14 +2,18 @@
 ### This library contains the common directories
 ###
 
+# Base directory
+# This is the root directory of the project
+BASE_DIR="$(realpath -L -P $(dirname "$0")/..)"
+
 # Stages directory
 # This directory contains ready-to-deploy packages (each in its stage directory)
-STAGES_DIR=$(realpath -L -P $(dirname "$0"))/../pkgs
+STAGES_DIR=$BASE_DIR/pkgs
 
 # Environment directory
 # This directory contains a virtual environment made up by the packages in
 # $STAGES_DIR
-ENV_DIR=$(realpath -L -P $(dirname "$0"))/../env
+ENV_DIR=$BASE_DIR/env
 
 # Installation directory
 # This directory contains the files and directories that will be included
@@ -18,29 +22,33 @@ INSTALL_DIR=/tmp/pkg
 
 # Builds directory
 # This directory contains all the build files
-BUILDS_DIR=$(realpath -L -P $(dirname "$0"))/../builds
+BUILDS_DIR=$BASE_DIR/builds
 
 # Cache directory
 # This directory contains the downloaded files of every package
-CACHE_DIR=$(realpath -L -P $(dirname "$0"))/../cache
+CACHE_DIR=$BASE_DIR/cache
 
 # Build directory
 # This directory contains the source files at compile time
-BUILD_DIR=$(realpath -L -P $(dirname "$0"))/../build
+BUILD_DIR=$BASE_DIR/build
 
 # Patch directory
 # This directory contains all the custom patch files
-PATCHES_DIR=$(realpath -L -P $(dirname "$0"))/../patches
+PATCHES_DIR=$BASE_DIR/patches
 
 # Other directory
 # This directory contains any other file that isn't suitable to be contained
 # in one of the other directories
-OTHER_DIR=$(realpath -L -P $(dirname "$0"))/../other
+OTHER_DIR=$BASE_DIR/other
 
 # Initramfs directory
 # This directory contains the files that will be added to the initramfs
-INITRAMFS_DIR=$(realpath -L -P $(dirname "$0"))/../initramfs
+INITRAMFS_DIR=$BASE_DIR/initramfs
 
 # Shared object dependencies log file
 # This file contains information about the shared objects needed for a package
 DEP_LOGFILE=/tmp/pkg_dependencies.log
+
+# Package database
+# This sqlite3 database contains information about built packages
+DB_PACKAGES=$BASE_DIR/packages.sqlite
