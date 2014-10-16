1191f93201c1b00b6269ea360972aa7e0a4f85fa
Alfredo Mungo <alfredo.mungo@yandex.com>
2014-10-17 01:30:49 +0200

    Now all stages are scanned at once (and results saved in the same DB)

--- scripts/gen-lib-db.py
+++ scripts/gen-lib-db.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+  gen-lib-db.py: Generates shared object database
+
+  Usage: gen-lib-db.py
+"""
 import sys
 import os
 import os.path
@@ -9,32 +14,37 @@ import tarfile
 DB_FILE="shared-objects.sqlite"
 
 def help():
-  print("Usage: {} STAGE#".format(sys.argv[0]), file=sys.stderr)
+  print("Usage: {}".format(sys.argv[0]), file=sys.stderr)
 
-if len(sys.argv) != 2:
-  help()
-  sys.exit(1)
-
-STAGE_DIR="pkgs/stage{}".format(sys.argv[1])
-
-if not os.path.exists(STAGE_DIR):
-  help()
-  sys.exit(1)
+def scan_stage(stage):
+  STAGE_DIR="pkgs/stage{}".format(stage)
+  
+  if not os.path.exists(STAGE_DIR):
+    print("Stage directory %s does not exist" % STAGE_DIR, file=sys.stderr)
+    sys.exit(1)
+  for dirpath, dirnames, filenames in os.walk(STAGE_DIR):
+    for f in filenames:
+      if f.endswith('.tar.xz'):
+        fpath = os.path.join(dirpath, f)
+        with tarfile.open(fpath, "r:xz") as tar:
+        
+          for tarf in tar.getmembers():
+            if tarf.isfile() and ('lib/' in tarf.name or 'lib64/' in tarf.name) and '.so' in tarf.name:
+              try:
+                package=f[:-len('.tar.xz')]
+                library=os.path.basename(tarf.name)
+                db.execute("INSERT INTO so(package, library, stage) VALUES('{package}','{library}', '{stage}')".format(package=package, library=library, stage=stage))
+              except sqlite3.IntegrityError:
+                print('WARNING: Integrity error with package="%s", library="%s", stage="%s"' % (package, library, stage), file=sys.stderr)
 
 db = sqlite3.connect(DB_FILE)
 
 db.execute("DROP TABLE IF EXISTS so")
-db.execute("CREATE TABLE so(package VARCHAR(256) NOT NULL, library VARCHAR(256) NOT NULL, PRIMARY KEY(package, library)) WITHOUT ROWID")
+db.execute("CREATE TABLE so(package VARCHAR(256) NOT NULL, library VARCHAR(256) NOT NULL, stage VARCHAR(4), PRIMARY KEY(package, library)) WITHOUT ROWID")
 
-for dirpath, dirnames, filenames in os.walk(STAGE_DIR):
-  for f in filenames:
-    if f.endswith('.tar.xz'):
-      fpath = os.path.join(dirpath, f)
-      with tarfile.open(fpath, "r:xz") as tar:
-      
-        for tarf in tar.getmembers():
-          if tarf.isfile() and ('lib/' in tarf.name or 'lib64/' in tarf.name) and '.so' in tarf.name:
-            db.execute("INSERT INTO so(package, library) VALUES('{package}','{library}')".format(package=f[:-len('.tar.xz')], library=os.path.basename(tarf.name)))
+for stage in '123P':
+  print('Scanning stage %s...' % stage)
+  scan_stage(stage)
 
 db.commit()
 db.close()
